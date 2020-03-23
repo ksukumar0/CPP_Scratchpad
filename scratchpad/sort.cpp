1c9bdc4326b93a3466486cf0ee9f3cc4a0479173
Karthik Sukumar <karthik.n.sukumar@gmail.com>
2020-03-23 22:20:55 +0100

    Sorting a custom vector added into sort.cpp

--- scratchpad/sort.cpp
+++ scratchpad/sort.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 #include <algorithm>
-//#include <bits/stdc++.h>
 #include <vector>
+
+class a
+{
+    public:
+        int b;
+        a(int value):b(value){};
+};
 
 static void printArray(std::vector<int> &values, const char *textToPrintBefore)
 {
@@ -13,9 +19,42 @@ static void printArray(std::vector<int> &values, const char *textToPrintBefore)
     std::cout<<'\n';
 }
 
+static void printVector(std::vector<a> &vect, const char *textToPrintBefore)
+{
+    std::cout<<textToPrintBefore<<'\n';
+    for(auto i: vect)
+    {
+        std::cout<<i.b<<' ';
+    }
+    std::cout<<'\n';
+}
+int compare(const a &first, const a &second)
+{
+//    if (first.b < second.b)
+//        return -1;
+//    else if (first.b > second.b)
+//        return 1;
+//    return 0;
+    return (first.b < second.b);
+}
+
 int sort_cpp_style()
 {
     std::vector<int> values{1341,12341,362,841,79,11,434,29,152,178,1};
+
+    std::vector<a> vecA;
+    for (auto i: values)
+    {
+        vecA.push_back(a(i));
+    }
+    std::cout<<"Size of Vector is "<<vecA.size()<<'\n';
+    std::cout<<"Sorting a Vector of Classes"<<'\n';
+    printVector(vecA,"Before Sorting");
+    std::sort(std::begin(vecA),std::end(vecA), compare);
+    printVector(vecA,"After Sorting");
+
+    std::cout<<"\n\n";
+    std::cout<<"Sorting Array of Ints"<<'\n';
     printArray(values,"Before Sorting");
     values.push_back(454);
     std::sort(std::begin(values), std::end(values));
