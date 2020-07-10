47831339f16ce355ee4f21b6d82e24bfb79f97b5
Karthik Sukumar <karthik.n.sukumar@gmail.com>
2020-07-10 15:37:50 +0200

    Comparing allocation on the stack vs allocation on the heap and scoping as well

--- scope/scope.cpp
+++ scope/scope.cpp
@@ -7,6 +7,9 @@ class Entity
         Entity() : m_Name("Unknown") {}
         Entity(const std::string& name) : m_Name(name) {}
 
+        ~Entity(){
+            std::cout<<"Destroying... "<<m_Name<<std::endl;
+        }
         const std::string& getName() const
         {
             return m_Name;
@@ -17,10 +20,14 @@ class Entity
 
 int main()
 {
-    Entity e = Entity();
+    Entity *e;
     Entity e1 = Entity("HiThere");
-
-    std::cout<<e.getName()<<std::endl;
-    std::cout<<e1.getName();
+    {
+        Entity entity = Entity("InScope");
+        //std::cout<<e.getName()<<std::endl;
+        e = &entity;
+        std::cout<<entity.getName()<<std::endl;
+    }
+    std::cout<<e->getName()<<std::endl;
     return 0;
 }
