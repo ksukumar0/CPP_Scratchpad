e9781107aa04d9a1e8de6dff76e8b85c941679d4
Karthik Sukumar <karthik.n.sukumar@gmail.com>
2020-11-18 10:24:25 +0100

    Singleton class example erweiterung

--- static/static_ex.cpp
+++ static/static_ex.cpp
@@ -8,6 +8,8 @@ private:
 public:
     static IDGenerator& getGen(int);
     int getNextID(); // Here's the declaration for a non-static function
+    IDGenerator & operator= (const IDGenerator & rhs) = delete;
+    IDGenerator (const IDGenerator &);
 };
 
 // Here's the definition of the static member outside the class.  Note we don't use the static keyword here.
@@ -16,12 +18,18 @@ public:
 
 IDGenerator& IDGenerator::getGen(int some_ID)
 {
-    static IDGenerator i(some_ID);
-    return i;
+    static IDGenerator initial(some_ID);
+    return initial;
 }
 
 IDGenerator::IDGenerator(int ID):s_nextID(ID){
+    std::cout<<"The constructor is called"<<std::endl;
 };
+
+IDGenerator::IDGenerator(const IDGenerator &rhs){
+    std::cout<<"The COPY constructor is called"<<std::endl;
+    s_nextID = rhs.s_nextID;
+}
 
 // Here's the definition of the static function outside of the class.  Note we don't use the static keyword here.
 int IDGenerator::getNextID()
@@ -32,7 +40,11 @@ int IDGenerator::getNextID()
 int main()
 {
     IDGenerator myID = IDGenerator::getGen(5);
+    IDGenerator myID1 = IDGenerator::getGen(15);
     for (int count{ 0 }; count < 5; ++count)
+    {
         std::cout << "The next ID is: " << myID.getNextID() << '\n';
+        std::cout << "The next ID1 is: " << myID1.getNextID() << '\n';
+    }
     return 0;
 }
