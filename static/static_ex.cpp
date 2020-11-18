fc365b7771ceb3443a553df97e373d1cad3c1494
Karthik Sukumar <sukumar@vites-gmbh.de>
2020-11-18 12:36:57 +0100

    Proper usage of singleton class

--- static/static_ex.cpp
+++ static/static_ex.cpp
@@ -9,14 +9,14 @@ public:
     static IDGenerator& getGen(int);
     int getNextID(); // Here's the declaration for a non-static function
     IDGenerator & operator= (const IDGenerator & rhs) = delete;
-    IDGenerator (const IDGenerator &);
+    IDGenerator (const IDGenerator &) = delete;
 };
 
 // Here's the definition of the static member outside the class.  Note we don't use the static keyword here.
 // We'll start generating IDs at some random number
 //int IDGenerator::s_nextID{ 1 };
 
-IDGenerator& IDGenerator::getGen(int some_ID)
+IDGenerator& IDGenerator::getGen(int some_ID = 0)
 {
     static IDGenerator initial(some_ID);
     return initial;
@@ -26,10 +26,12 @@ IDGenerator::IDGenerator(int ID):s_nextID(ID){
     std::cout<<"The constructor is called"<<std::endl;
 };
 
-IDGenerator::IDGenerator(const IDGenerator &rhs){
-    std::cout<<"The COPY constructor is called"<<std::endl;
-    s_nextID = rhs.s_nextID;
-}
+// If you have the copy constructor then it can be erroneous for a Singleton class
+
+//IDGenerator::IDGenerator(const IDGenerator &rhs){
+//    std::cout<<"The COPY constructor is called"<<std::endl;
+//    s_nextID = rhs.s_nextID;
+//}
 
 // Here's the definition of the static function outside of the class.  Note we don't use the static keyword here.
 int IDGenerator::getNextID()
@@ -39,12 +41,10 @@ int IDGenerator::getNextID()
 
 int main()
 {
-    IDGenerator myID = IDGenerator::getGen(5);
-    IDGenerator myID1 = IDGenerator::getGen(15);
+    IDGenerator::getGen(5);
     for (int count{ 0 }; count < 5; ++count)
     {
-        std::cout << "The next ID is: " << myID.getNextID() << '\n';
-        std::cout << "The next ID1 is: " << myID1.getNextID() << '\n';
+        std::cout << "The next ID is: " << IDGenerator::getGen().getNextID() << '\n';
     }
     return 0;
 }
