10b9c8ce203aa7e5c51dbc42219edb4696e0c666
Karthik Sukumar <karthik.n.sukumar@gmail.com>
2021-04-09 18:19:44 +0200

    Test for checking QtFile ops

---
+++ scratchtest/test.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string>
+#include <cassert>
+#include <cstdbool>
+#include <QFile>
+
+bool copyFile(const char *src, const char *dest, bool failIfExists)
+{
+   if (QFile::exists(dest))
+   {
+      if (failIfExists)
+      {
+         return false;
+      }
+      else
+      {
+         QFile::remove(dest);
+      }
+   }
+   QFile::copy(src, dest);
+   return true;
+}
+
+#define BUF_SIZE 1000
+int main(void)
+{
+   char buf[BUF_SIZE];
+   char cwd[BUF_SIZE];
+   getcwd(cwd, BUF_SIZE);
+
+   printf("%s\n",cwd);
+   //char file1 = "test1";
+   //char file2 = "test2";
+
+   //const char *src = strncat(cwd, file1);
+   //const char *dest = strncat();
+   //bool ret = copyFile(src, dest, TRUE);
+}
