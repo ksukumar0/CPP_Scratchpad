90e0c04a403bb8957e3d7a4eddbd8b742194a05c
Karthik Sukumar <karthik.n.sukumar@gmail.com>
2021-04-12 10:26:40 +0200

    Test program Implemented with asserts for QFile::copy()

--- scratchtest/test.cpp
+++ scratchtest/test.cpp
@@ -5,36 +5,59 @@
 #include <cstdbool>
 #include <QFile>
 
+#define BUF_SIZE 1000
+
 bool copyFile(const char *src, const char *dest, bool failIfExists)
 {
-   if (QFile::exists(dest))
-   {
-      if (failIfExists)
-      {
-         return false;
-      }
-      else
-      {
-         QFile::remove(dest);
-      }
-   }
-   QFile::copy(src, dest);
-   return true;
+    if (QFile::exists(dest))
+    {
+        if (failIfExists)
+        {
+            return false;
+        }
+        else
+        {
+            QFile::remove(dest);
+        }
+    }
+    QFile::copy(src, dest);
+    return true;
 }
 
-#define BUF_SIZE 1000
+static void formPath(const char * fileName, char * cwd, char * pathToFile)
+{
+    strncpy(pathToFile, cwd, BUF_SIZE);
+    strncat(pathToFile, "/", 2);
+    strncat(pathToFile, fileName, BUF_SIZE);
+
+    printf("File1 is %s\n",pathToFile);
+}
+
 int main(void)
 {
-   char buf[BUF_SIZE];
-   char cwd[BUF_SIZE];
-   getcwd(cwd, BUF_SIZE);
+    char src[BUF_SIZE];
+    char dest[BUF_SIZE];
 
-   printf("CWD is %s\n",cwd);
-   char file1[] = "test1";
-   char file2[] = "test2";
+    char cwd[BUF_SIZE];
+    getcwd(cwd, BUF_SIZE);
 
-   strncat(cwd, "/", 2);
-   strncat(cwd, file1, BUF_SIZE);
-   printf("File1 is %s\n",cwd);
-   //bool ret = copyFile(src, dest, TRUE);
+    char file1[] = "test1";
+    char file2[] = "test2";
+
+    formPath(file1, cwd, src);
+    formPath(file2, cwd, dest);
+
+    bool ret = copyFile(src, dest, true);
+
+    // test2 exists and cant be copied SHOULD return false
+    assert(ret == false);
+
+    ret = copyFile(src, dest, false);
+    // test2 exist and can be copied SHOULD return true
+    assert(ret == true);
+
+    QFile::remove(dest);
+    ret = copyFile(src, dest, false);
+    // test2 exist and can be copied SHOULD return true
+    assert(ret == true);
 }
