04197fdc4fd89cb5d1fb8986829ba0c3f8259ac4
Karthik Sukumar <karthik.n.sukumar@gmail.com>
2021-04-23 10:59:26 +0200

    Added standard comparisons for enum initialisation

--- templates/template.cpp
+++ templates/template.cpp
@@ -7,7 +7,7 @@ template<int v> struct Int2Type {
     };
 };
 
-enum test {
+enum class test {
     RED = 12,
     BLUE,
     GREEN
@@ -16,9 +16,11 @@ enum test {
 int main()
 {
     auto ch = Int2Type<test::GREEN>();
-    enum test t = static_cast<enum test>(17);
-    std::cout<<t;
-    (void) t;
     (void) ch;
+    test t = static_cast<test>(17);
+    test t2{24};
+    //std::cout<<t;
+    (void) t;
+    (void) t2;
 }
 
