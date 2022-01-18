5599302b441a8572f6b0b0ba4fa497afa9d36b7a
Karthik Sukumar <karthik.n.sukumar@gmail.com>
2022-01-18 22:01:55 +0100

    Move semantics: rvalue copy constructor written

--- moveSemantics/moveSemantics.cpp
+++ moveSemantics/moveSemantics.cpp
@@ -6,16 +6,28 @@ using namespace std;
 class func
 {
     public:
-        func():m_count(0){};
-        func(int a):m_count(a){};
+        func():m_count(0){
+            space = nullptr;
+        };
+        func(int a):m_count(a){
+            space = nullptr;
+        };
         func(func & t)
         {
             cout<<"In copy constructor"<<endl;
             this->m_count = t.m_count;
+            space = new int;
         }
         func & operator=(const func &rhs)
         {
             cout<<"In assignment operator"<<endl;
+            return *this;
+        }
+        func& operator=(func&& rhs)
+        {
+            delete space;
+            this->m_count = rhs.m_count;
+            this->space = rhs.space;
             return *this;
         }
         void print()
@@ -24,6 +36,7 @@ class func
         }
     private:
         int m_count;
+        int *space;
 };
 
 int main()
@@ -33,5 +46,6 @@ int main()
     func *h = new func(23);
     t.print();
     h->print();
+
     return 0;
 }
