7b96099f3da6cba0e4a4c8ccb467a92975cce1dc
Karthik Sukumar <karthik.sukumar@rohde-schwarz.com>
2022-06-03 15:06:32 +0200

    Remenants

--- QtTest/QlibTest.cpp
+++ QtTest/QlibTest.cpp
@@ -6,6 +6,7 @@
 #include <QFile>
 #include <QDebug>
 #include <QLibrary>
+#include <dlfcn.h>
 
 #define BUF_SIZE 1000
 
@@ -23,39 +24,42 @@ int main(void)
     //char cwd[BUF_SIZE];
     //getcwd(cwd, BUF_SIZE);
 
-    QString libName{"libRsMeasDriver.so"};
+    QString libName{"/home/sukumar/Projects/ESMAIN/sukumar_ES-MAIN_21.4.16.0_view/Build/clang_x64/bin/binDebug/libRsMeasDriver.so"};
+    QString libFswName{"/home/sukumar/Projects/ESMAIN/sukumar_ES-MAIN_21.4.16.0_view/Build/clang_x64/bin/binDebug/libFswMeasDriver.so"};
     QLibrary componentLibrary( libName );
+    QLibrary componentLibrary1( libFswName );
 
     bool b = componentLibrary.isLibrary(libName);
+    //bool b1 = componentLibrary1.isLibrary(libFswName);
+    bool b1;
+    void *test = dlopen("libFswMeasDriver", RTLD_NOW);
+
 
     // Is it a lib
     if(b)
        std::cout<<"is Lib"<<std::endl;
     else
        std::cout<<"is Not Lib"<<std::endl;
-   
-    // Whats the load status
-    auto a = static_cast<int>(componentLibrary.loadHints());
-    printf("%d\n",a);
 
-    std::cout<<componentLibrary.errorString().toStdString()<<std::endl;
+    // Whats the load status
+
     if (componentLibrary.load())
     {
-       std::cout <<"Loaded";
+       std::cout <<"Loaded\n";
     }
     else
     {
-       std::cout<<"Not Loaded";
+       std::cout<<"Not Loaded\n";
     }
-//    // test2 exists and cant be copied SHOULD return false
-//    assert(ret == false);
-//
-//    ret = copyFile(src, dest, false);
-//    // test2 exist and can be copied SHOULD return true
-//    assert(ret == true);
-//
-//    QFile::remove(dest);
-//    ret = copyFile(src, dest, false);
-//    // test2 exist and can be copied SHOULD return true
-//    assert(ret == true);
+
+    if (!test)
+    {
+       std::cout << "FswMeasDriver returned Null\n";
+    }
+    else
+    {
+       std::cout<< "FswMeasDriver Successful\n";
+    }
+    std::cout<<componentLibrary.errorString().toStdString()<<std::endl;
+    (void) b1;
 }
