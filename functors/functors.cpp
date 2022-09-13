183b8e8ddf48ff0b784e70f1a459aa554c59b409
Karthik Sukumar <karthik.sukumar@rohde-schwarz.com>
2022-09-13 07:19:20 +0200

    function typedefs

--- functors/functors.cpp
+++ functors/functors.cpp
@@ -4,24 +4,34 @@
 #include<string>
 
 using namespace std;
+typedef uint64_t (*intret_t) (int, char*);
 
 class func
 {
-    public:
-        func(int mult) : m_multiplier(mult){
-        }
-        int operator () (int toMult)
-        {
-            return m_multiplier*toMult;
-        }
-    private:
-        int m_multiplier;
+   public:
+      func(int mult) : m_multiplier(mult){
+      }
+      int operator () (int toMult)
+      {
+         return m_multiplier*toMult;
+      }
+   private:
+      int m_multiplier;
 };
+
+static uint64_t testFunc(int numBuf, char *buffer)
+{
+   cout<<numBuf<<" "<<buffer;
+   return numBuf*numBuf;
+}
 
 int main()
 {
-    func mul8(8), mul64(64);
-    cout<<"6*8 is "<<mul8(6)<<endl;
-    cout<<"6*64 is "<<mul64(6)<<endl;
-    return 0;
+   intret_t test = &testFunc;
+   char teststr[] = "Howdy\n";
+   test(1,teststr);
+   func mul8(8), mul64(64);
+   cout<<"6*8 is "<<mul8(6)<<endl;
+   cout<<"6*64 is "<<mul64(6)<<endl;
+   return 0;
 }
