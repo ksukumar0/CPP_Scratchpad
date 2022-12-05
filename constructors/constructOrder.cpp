f5eb9deb9b8273d1308a9f3f53c62e9639cf1b50
Karthik Sukumar <karthik.sukumar@rohde-schwarz.com>
2022-12-05 12:19:12 +0100

    Inheritance constructor order

---
+++ constructors/constructOrder.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+struct V {
+  V() { cout << "V()" << endl; }
+};
+struct V2 {
+  V2() { cout << "V2()" << endl; }
+};
+struct A {
+  A() { cout << "A()" << endl; }
+};
+struct B : virtual V {
+  B() { cout << "B()" << endl; }
+};
+struct C : B, virtual V2 {
+  C() { cout << "C()" << endl; }
+};
+struct D : C, virtual V {
+  A obj_A;
+  D() { cout << "D()" << endl; }
+};
+
+struct E : V {
+  E() { cout << "E()" << endl; }
+};
+
+int main() {
+  D c;
+  std::cout<<"PART II\n";
+  E e;
+}
