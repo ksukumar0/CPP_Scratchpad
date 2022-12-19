81faecbd8dd1ee5abdacc286a456c76a46f1fde8
Karthik Sukumar <karthik.sukumar@rohde-schwarz.com>
2022-12-19 14:48:44 +0100

    Added custom string to mangle

--- biosVersion/biosVersion.cpp
+++ biosVersion/biosVersion.cpp
@@ -5,12 +5,34 @@
 #include <stdio.h>
 #include "biosVersion.h"
 
+void mangleText(std::string text)
+{
+   std::string sub = "IPY20";
+   std::cout<<text;
+   std::cout<<text.find(sub)<<std::endl;
+   const std::string start = text.substr(sub.length());
+   const std::string end = text.substr(0,sub.length());
+   std::cout<<"start: "<<start<<std::endl;
+   std::cout<<"end: "<<end<<std::endl;
+   std::string full = "R&S Analyzer BIOS " + start + " " + end;
+   std::cout<<"full: "<<full<<std::endl;
+
+}
+
 void openinCPP(std::string& path)
 {
    std::ifstream readFile(path);
-   std::stringstream buffer;
-   buffer << readFile.rdbuf();
-   std::cout<<buffer.str();
+   if(!readFile.fail())
+   {
+      std::stringstream buffer;
+      buffer << readFile.rdbuf();
+      std::cout<<buffer.str();
+      mangleText("IPY20E111.02");
+   }
+   else
+   {
+      printf("File not found\n");
+   }
 }
 
 int main()
@@ -23,12 +45,12 @@ int main()
    {
       [[maybe_unused]] int retval = fscanf(fp, "%s", biosVersion);
       printf("%s\n",biosVersion);
-      openinCPP(path);
    }
    else
    {
       printf("File not found\n");
    }
+   openinCPP(path);
 
    return 0;
 }
