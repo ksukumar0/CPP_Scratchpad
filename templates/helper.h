c77b33bc967bdd809f5702e57ab5610a45709bd4
Karthik Sukumar <karthik.sukumar@rohde-schwarz.com>
2023-01-24 11:31:59 +0100

    Template Meta Programming small example

--- templates/helper.h
+++ templates/helper.h
@@ -1,13 +1,14 @@
 namespace XHW{
    class CONTROL{
-      static constexpr const int s_sigMiq_on  = 0;
-      static constexpr const int s_sigIn1_on  = 1;
-      static constexpr const int s_sigOut1_on = 2;
-      static constexpr const int s_sigOut2_on = 3;
-      static constexpr const int s_sigOut3_on = 4;
-      static constexpr const int s_sigContmem_on = 5;
+      public:
+         static constexpr const int s_sigMiq_on  = 0;
+         static constexpr const int s_sigIn1_on  = 1;
+         static constexpr const int s_sigOut1_on = 2;
+         static constexpr const int s_sigOut2_on = 3;
+         static constexpr const int s_sigOut3_on = 4;
+         static constexpr const int s_sigContmem_on = 5;
 
-      enum class MIQ_ON: uint32_t
+         enum class MIQ_ON: uint32_t
       {
          /** Signal value OFF: Disabled, not active. **/
          OFF = 0,
@@ -15,7 +16,7 @@ namespace XHW{
          ON = 1
       };
 
-      enum class IN1_ON: uint32_t
+         enum class IN1_ON: uint32_t
       {
          /** Signal value OFF: Disabled, not active. **/
          OFF = 0,
@@ -23,7 +24,7 @@ namespace XHW{
          ON = 1
       };
 
-      enum class OUT1_ON: uint32_t
+         enum class OUT1_ON: uint32_t
       {
          /** Signal value OFF: Disabled, not active. **/
          OFF = 0,
@@ -31,7 +32,7 @@ namespace XHW{
          ON = 1
       };
 
-      enum class OUT2_ON: uint32_t
+         enum class OUT2_ON: uint32_t
       {
          /** Signal value OFF: Disabled, not active. **/
          OFF = 0,
@@ -39,7 +40,7 @@ namespace XHW{
          ON = 1
       };
 
-      enum class OUT3_ON: uint32_t
+         enum class OUT3_ON: uint32_t
       {
          /** Signal value OFF: Disabled, not active. **/
          OFF = 0,
@@ -47,7 +48,7 @@ namespace XHW{
          ON = 1
       };
 
-      enum class CONTMEM_ON: uint32_t
+         enum class CONTMEM_ON: uint32_t
       {
          /** Signal value OFF: Disabled, not active. **/
          OFF = 0,
@@ -55,16 +56,25 @@ namespace XHW{
          ON = 1
       };
 
-      template <class ValType>
-         static constexpr const int * GetSignalByType()
-         {
-            if constexpr (std::is_same<ValType, MIQ_ON>::value) return &s_sigMiq_on;
-            if constexpr (std::is_same<ValType, IN1_ON>::value) return &s_sigIn1_on;
-            if constexpr (std::is_same<ValType, OUT1_ON>::value) return &s_sigOut1_on;
-            if constexpr (std::is_same<ValType, OUT2_ON>::value) return &s_sigOut2_on;
-            if constexpr (std::is_same<ValType, OUT3_ON>::value) return &s_sigOut3_on;
-            if constexpr (std::is_same<ValType, CONTMEM_ON>::value) return &s_sigContmem_on;
-            return nullptr;
-         }
+         template <class ValType>
+            static constexpr const int * GetSignalByType()
+            {
+               if constexpr (std::is_same<ValType, MIQ_ON>::value) return &s_sigMiq_on;
+               if constexpr (std::is_same<ValType, IN1_ON>::value) return &s_sigIn1_on;
+               if constexpr (std::is_same<ValType, OUT1_ON>::value) return &s_sigOut1_on;
+               if constexpr (std::is_same<ValType, OUT2_ON>::value) return &s_sigOut2_on;
+               if constexpr (std::is_same<ValType, OUT3_ON>::value) return &s_sigOut3_on;
+               if constexpr (std::is_same<ValType, CONTMEM_ON>::value) return &s_sigContmem_on;
+               return nullptr;
+            }
+
+         template<class SignalValType, class = std::enable_if_t<GetSignalByType<SignalValType>() != nullptr>>
+            CONTROL& operator << (const SignalValType value)
+            {
+               constexpr const int &signal = *(GetSignalByType<SignalValType>());
+               std::cout<<"Signal value is "<<signal<<"\n";
+               return *this;
+            }
+
    };
 };
--- templates/template.cpp
+++ templates/template.cpp
@@ -3,28 +3,34 @@
 #include "helper.h"
 #define RL_SUPPORT_SERVICE 1
 
-template<typename v> struct Int2Type {
-    enum class xyz{
-        value = v
-    };
-};
-
 enum class test {
     RED = 12,
     BLUE,
     GREEN
 };
 
+template<typename v> struct Int2Type {
+//   test value;
+//   enum {
+//      value = v
+//   };
+};
+
 void test1(void)
 {
+   XHW::CONTROL t;
+   std::cout<<*(t.GetSignalByType<XHW::CONTROL::OUT3_ON>())<<std::endl;
+   std::cout<<t.GetSignalByType<int>()<<std::endl; // Should return nullptr
+   t<<XHW::CONTROL::OUT2_ON::ON;
+
    return;
 }
 
 int main()
 {
-    [[maybe_unused]]auto ch = Int2Type<test::GREEN>();
+    [[maybe_unused]] auto ch = Int2Type<test>();
     [[maybe_unused]] test t = static_cast<test>(17);
     [[maybe_unused]] test t2{24};
-    //std::cout<<t;
+    test1();
 }
 
