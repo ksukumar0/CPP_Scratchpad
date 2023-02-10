1d59487bcadc5959af371a3f286f7b520fa08e4a
Karthik Sukumar <karthik.sukumar@rohde-schwarz.com>
2023-02-10 13:38:10 +0100

    Edited the destOrderInhtce.cpp file to see see the constructor calling order for derived classes

--- constructors/destOrderInhtce.cpp
+++ constructors/destOrderInhtce.cpp
@@ -1,24 +1,24 @@
 #include <iostream>
 class A 
 {
-    public:
-    A() { name="untitled"; std::cout << name <<" constructor" << std::endl; }
-    A(std::string name):name(name) { std::cout << name <<" constructor" << std::endl; }
-    ~A() { std::cout << name <<" destructor" << std::endl; }
-    private:
-    std::string name;
+   public:
+      A() { name="untitled"; std::cout << name <<" constructor" << std::endl; }
+      A(std::string name):name(name) { std::cout << name <<" constructor" << std::endl; }
+      ~A() { std::cout << name <<" destructor" << std::endl; }
+      std::string name;
 };
 
 class B : public A
 {
-    public:
-    A a, aa, aaa;
-    B() : A("A "),a("a"),aa("aa"){ std::cout << "B constructor" << std::endl; }
-    ~B() { std::cout << "B destructor" << std::endl; }
+   public:
+      //A a, aa, aaa;
+      //B() : A("A "),a("a"),aa("aa"){ std::cout << "B constructor" << std::endl; }
+      B(): A("Testing one"){std::cout << "B constructor" << std::endl;}
+      ~B() { std::cout << "B destructor" << std::endl; }
 };
 B beta;
 
 int main()
 {
-    return 0;
+   return 0;
 }
