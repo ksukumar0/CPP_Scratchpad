bfc22fa0fcb8c4f7e4130441ec59c4fefcd069b3
Karthik Sukumar <karthik.sukumar@rohde-schwarz.com>
2023-02-10 13:59:10 +0100

    reformating

--- union/union.cpp
+++ union/union.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 
-typedef union
+union testBits
 {
    struct  __attribute__((packed))  {
       uint8_t lsb : 3;
       uint8_t msb : 5;
    }bits;
    uint8_t flags;
-}testBits_t;
+};
 
 int main() {
-   testBits_t t;
+   testBits t;
    t.bits.lsb = 7;
    t.bits.msb = 30;
 
-   printf("sizeof(testBits_t) = %lu\n", sizeof(t));
+   printf("sizeof(testBits) = %lu\n", sizeof(t));
    printf("LSB = %d\n", t.bits.lsb);
    printf("MSB = %d\n", t.bits.msb);
    printf("FLAG = %d\n", t.flags);
