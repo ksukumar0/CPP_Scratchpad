e23f6dee706712218bfd818eff37b727b23862d2
Karthik Sukumar <karthik.sukumar@rohde-schwarz.com>
2023-05-03 11:27:26 +0200

    changes in sharedObj and union with more members to test use case for context info

--- union/union.cpp
+++ union/union.cpp
@@ -1,22 +1,58 @@
 #include <iostream>
+#include <vector>
 
 union testBits
 {
+   uint8_t flags;
    struct  __attribute__((packed))  {
       uint8_t lsb : 3;
       uint8_t msb : 5;
    }bits;
-   uint8_t flags;
 };
 
+class unionClass
+{
+   public:
+      unionClass()
+      {
+         //m_ctxMemUnion.m_ctxVector = ctxData;
+      }
+
+   private:
+      union test{
+         test(){}
+         ~test(){}
+         std::vector<uint8_t> m_ctxVector;
+         struct __attribute__((packed)) m_ctxMemBytes{
+            uint64_t m_tspStart;      // 16 Bytes
+            uint8_t m_marker;             //  1 Byte
+            uint16_t m_bitIndex;          //  2 Bytes
+            uint8_t m_blockIndex;         //  1 Byte
+            uint32_t m_addrSeg;           //  4 Bytes
+            uint64_t tspStart;        // 16 Bytes
+            uint64_t tspStop;         // 16 Bytes
+         };
+      }m_ctxMemUnion;
+};
+
+void testunionclass()
+{
+   [[maybe_unused]] unionClass u;
+   //u.m_ctxMemUnion.m_ctxMemBytes.m_tspStart = 10;
+   //u.m_ctxMemUnion.m_ctxMemBytes.m_tspStop = 22;
+}
+
 int main() {
+
+   testunionclass();
+
    testBits t;
    t.bits.lsb = 7;
    t.bits.msb = 30;
 
    printf("sizeof(testBits) = %lu\n", sizeof(t));
-   printf("LSB = %d\n", t.bits.lsb);
-   printf("MSB = %d\n", t.bits.msb);
-   printf("FLAG = %d\n", t.flags);
+   printf("LSB = %x\n", t.bits.lsb);
+   printf("MSB = %x\n", t.bits.msb);
+   printf("FLAG = %x\n", t.flags);
    return 0;
 }
--- useSharedObj/main.cpp
+++ useSharedObj/main.cpp
@@ -1,5 +1,6 @@
 #include "triangle.hpp"
 #include <iostream>
+#include <cxxabi.h>
 
 // Website for dynamic Libs in CPP
 //https://tldp.org/HOWTO/C++-dlopen/index.html
@@ -9,9 +10,15 @@ int main() {
     using std::cerr;
 
     // create an instance of the class
-    polygon* poly = new triangle();
+    polygon * poly = new triangle();
 
     // use the class
     poly->set_side_length(7);
     cout << "The area is: " << poly->area() << '\n';
+
+    int status;
+    char * demangled = abi::__cxa_demangle(typeid(poly).name(),0,0,&status);
+    cout<<"Name of Polygon is: "<<typeid(poly).name()<<std::endl;
+    cout<<"Demangled Type is: "<<demangled<<std::endl;
+    free(demangled);
 }
